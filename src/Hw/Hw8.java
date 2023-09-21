00ea5265d506389fbc7f91b8cfbe0a2bef6629fe
cloudz3r0 <ren7ero@gmail.com>
2023-09-21 19:06:29 -0400

    hi

--- src/Hw/Hw8.java
+++ src/Hw/Hw8.java
@@ -5,15 +5,12 @@ public class Hw8 {
     Write a Java Function that increments a string,
 It should take the string of unknown length. Don't use regular
 expressions.
-     */
-    public static String Increment(String str) {
+     */public static String Increment(String str) {
         int num = Integer.valueOf(str);
         num++;
 
         return Integer.toString(num);
     }
-
-
     public static void main(String[] args) {
         String input1 = "123";
         String input2 = "7673562";
